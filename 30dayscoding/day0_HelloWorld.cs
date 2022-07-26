9f6629bbc204c8ab5afe687210d4c52cac868416
Franco459 <francopistonecarp11@gmail.com>
2022-07-26 12:36:03 -0300

    primer ejercicio 30 days of code challenge

---
+++ 30dayscoding/day0_HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    static void Main(String[] args) {
+        String inputString; 
+        
+        inputString = Console.ReadLine(); 
+        
+        Console.WriteLine("Hello, World.\n"+inputString );
+        
+    }
+}
