5af4a9ace0fb441d0dcc6e5fb4a0f938236bd2e4
Franco459 <francopistonecarp11@gmail.com>
2022-07-31 18:48:13 -0300

    day5 of 30 days coding

---
+++ 30dayscoding/day5_loops.cs
@@ -0,0 +1,26 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        for (int i = 0; i < 10; i++){
+            Console.WriteLine(n +" x " + (i+1) + " = " + n*(i+1));
+        }
+    }
+}
