bb9913e96cb81b7664188639749eddcbc6f7b585
Franco459 <francopistonecarp11@gmail.com>
2022-08-24 15:56:00 -0300

    day 16 solution java

--- 30dayscoding/day16_exceptions.java
+++ 30dayscoding/day16_exceptions.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-public class Solution {
+public class day16_exceptions {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
