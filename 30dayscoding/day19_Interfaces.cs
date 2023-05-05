673c8fcdc2f64bb7988a568a481eed593f53e601
Franco459 <francopistonecarp11@gmail.com>
2023-05-05 16:20:15 -0300

    dia 19, interfaces

---
+++ 30dayscoding/day19_Interfaces.cs
@@ -0,0 +1,25 @@
+using System;
+public interface AdvancedArithmetic{
+    int divisorSum(int n);
+}
+
+public class Calculator : AdvancedArithmetic
+{
+    public int divisorSum(int n)
+    {
+        int div = 0;
+        for(int i = 0; i < n; i++){
+            if(n % (i + 1) == 0) div += (i+1);
+        }
+        return div;
+    }
+}
+
+class Solution{
+    static void Main(string[] args){
+        int n = Int32.Parse(Console.ReadLine());
+      	AdvancedArithmetic myCalculator = new Calculator();
+        int sum = myCalculator.divisorSum(n);
+        Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum); 
+    }
+}
