0e2f5f6f4f176b34f0ce859603647549b0bb74aa
Franco459 <francopistonecarp11@gmail.com>
2024-05-19 11:02:52 -0300

    soluciones en c#

---
+++ othersolutions/codeforces-Complicating-passwords.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace MyCompiler {
+    class Program {
+        public static void Main(string[] args) {
+            
+            
+            
+            /*input*/
+            /*String str = Console.;
+      
+            String[] spearator = {" "};
+            Int32 count = 2;
+      
+            // using the method
+            String[] strlist = str.Split(spearator,
+                   StringSplitOptions.RemoveEmptyEntries);
+      
+            foreach(String s in strlist)
+            {
+                Console.WriteLine(s);
+            }
+            /*end input*/
+            int first = 4;
+            int second = 6;
+            int newNum = 0;
+            int[] inputNum;
+            if (num)
+            for(int i = 0; i < 10; i++){
+                newNum = first * second;
+                if(newNum > 9){
+                    Console.WriteLine((newNum/10) + " " + newNum%10);
+                    first = newNum / 10;
+                    second = newNum % 10;
+                }
+                else{
+                    first = second;
+                    second  = newNum;
+                }
+            }
+        }
+    }
+}
